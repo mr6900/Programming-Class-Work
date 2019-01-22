d8e1c44b23dee057392d0a1b574a1159246695a1
mr6900 <mr6900@students.inghamisd.org>
2019-01-22 09:31:04 -0500

    Class work

---
+++ AdvObjects/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+//most of this assignment is trying to read the existing code to understand how it works,
+        //ask a classmate or instructor if you are unsure how the code works.
+
+
+        //generate a new player
+        
+        //create main game loop that continues until the player has no health
+            //generate a new enemy( the level of the enemy should be the player's level)
+            //create battle loop that should continue while both the player and enemy are alive
+                //gets player's input on what to do (use one of the methods to do this)
+                //has the enemy take damage
+                //has the player take damage
+        //once the player is dead print out the player's level and how much xp they had.
+    }
+}
